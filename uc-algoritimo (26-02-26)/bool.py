fd865b299a0f82ac4b2a456571f42dc254738423
thi2010 <thiagocdebrito2010@gmail.com>
2026-02-26 14:36:25 -0300

    Add files via upload

---
+++ uc-algoritimo (26-02-26)/bool.py
@@ -0,0 +1,6 @@
+resp = input("Você vai passar de ano? S/N").strip().lower()
+#resposta = bool(resp)
+resposta = (resp == "S")
+
+print("Resposta do aluno", resp)
+print("Resultado", resposta)
---
+++ uc-algoritimo (26-02-26)/exemplo.py
@@ -0,0 +1,7 @@
+aluno = input("Digite seu nome:")
+periodo = input("Digite em que periodo você está: ")
+ira = input("Digite seu IRA:")
+
+print(type(aluno))
+print(type(periodo))
+print(type(ira))
